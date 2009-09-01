553c4f5dc002e434f19170b6f0431bdbb5e9db19
smly <smly@blei.(none)>
2009-09-02 02:25:50 +0900

    modify interface

--- Hubigraph.hs
+++ Hubigraph.hs
@@ -1,17 +1,10 @@
 module Hubigraph (
    module Hubigraph.Base,
+   --module Hubigraph.Old,
    initHubigraph, runHubigraph,
-   clear, newVertexWithID, newVertex, removeVertex,
+   clear,
+   newVertexWithID, newVertex, removeVertex,
    newEdgeWithID, newEdge,
-   vertexShape, vertexShapedetail, vertexSize,
-   vertexColor, vertexLabel, vertexFontcolor,
-   vertexFontfamily, vertexFontsize, vertexVisible,
-   edgeColor, edgeLabel, edgeFontcolor,
-   edgeFontfamily, edgeFontsize, edgeSpline,
-   edgeStrength, edgeOriented, edgeStroke,
-   edgeArrow, edgeArrowPosition,
-   edgeArrowRadius, edgeArrowLength, edgeArrowReverse,
-   edgeShowstrain, edgeVisible, edgeWidth,
   ) where
 
 import Control.Monad.Reader (ReaderT(..), runReaderT, asks, liftIO, lift)
@@ -20,125 +13,116 @@ import Data.Char (toLower)
 
 import Hubigraph.Base
 
+{-
+typedef int32_t result_t;
+typedef int32_t vertex_id_t;
+typedef int32_t edge_id_t;
+typedef int32_t style_id_t;
+
+/* Basic API methods */
+vertex_id_t ubigraph_new_vertex();
+edge_id_t   ubigraph_new_edge(vertex_id_t x, vertex_id_t y);
+result_t    ubigraph_remove_vertex(vertex_id_t x);
+result_t    ubigraph_remove_edge(edge_id_t e);
+
+/* Vertex/edge creation when user wants to use their own id's */
+result_t    ubigraph_new_vertex_w_id(vertex_id_t x);
+result_t    ubigraph_new_edge_w_id(edge_id_t e, vertex_id_t x, vertex_id_t y);
+
+/* Delete all vertices and edges */
+void        ubigraph_clear();
+
+/* Set a vertex attribute */
+result_t    ubigraph_set_vertex_attribute(vertex_id_t x,
+              const char* attribute, const char* value);
+
+/* Vertex styles */
+result_t    ubigraph_change_vertex_style(vertex_id_t x, style_id_t s);
+style_id_t  ubigraph_new_vertex_style(style_id_t parent_style);
+result_t    ubigraph_new_vertex_style_w_id(style_id_t s, 
+              style_id_t parent_style);
+result_t    ubigraph_set_vertex_style_attribute(style_id_t s,
+              const char* attribute, const char* value);
+
+/* Set an edge attribute */
+result_t    ubigraph_set_edge_attribute(edge_id_t x,
+              const char* attribute, const char* value);
+
+/* Edge styles */
+result_t    ubigraph_change_edge_style(edge_id_t x, style_id_t s);
+style_id_t  ubigraph_new_edge_style(style_id_t parent_style);
+result_t    ubigraph_new_edge_style_w_id(style_id_t s,
+              style_id_t parent_style);
+result_t    ubigraph_set_edge_style_attribute(style_id_t s,
+              const char* attribute, const char* value);
+-}
+
+-- for debug
+r x = initHubigraph "http://localhost:20738/RPC2" >>= runHubigraph x
+
 runHubigraph = runReaderT
 
 initHubigraph :: (Monad m) => String -> m Ubigraph
 initHubigraph serv = return ( Ubigraph { server = serv } )
 
-clear :: Hubigraph Int
+toBool :: IO Int -> IO Bool
+toBool x = do
+  x' <- x
+  return $ if x' == 0 then True else False
+
+clear :: Hubigraph Bool
 clear =
     do serv <- asks server
-       liftIO $ remote serv "ubigraph.clear"
+       liftIO . toBool $ remote serv "ubigraph.clear"
 
-newVertexWithID :: ID -> Hubigraph Int
+newVertexWithID :: VertexID -> Hubigraph Bool
 newVertexWithID node =
     do serv <- asks server
-       liftIO $ remote serv "ubigraph.new_vertex_w_id" node
+       liftIO . toBool $ remote serv "ubigraph.new_vertex_w_id" node
 
-newVertex :: Hubigraph Int
+newVertex :: Hubigraph VertexID
 newVertex =
     do serv <- asks server
        liftIO $ remote serv "ubigraph.new_vertex"
 
-removeVertex :: Hubigraph Int
-removeVertex =
+removeVertex :: VertexID -> Hubigraph Bool
+removeVertex vid =
     do serv <- asks server
-       liftIO $ remote serv "ubigraph.remove_vertex"
+       liftIO . toBool $ remote serv "ubigraph.remove_vertex" vid
 
-newEdgeWithID :: ID -> Edge -> Hubigraph Int
+newEdgeWithID :: EdgeID -> Edge -> Hubigraph Bool
 newEdgeWithID eid (src,dst) =
     do serv <- asks server
-       liftIO $ remote serv "ubigraph.new_edge_w_id" eid src dst
+       liftIO . toBool $ remote serv "ubigraph.new_edge_w_id" eid src dst
 
-newEdge :: Edge -> Hubigraph Int
+newEdge :: Edge -> Hubigraph EdgeID
 newEdge (src,dst) =
     do serv <- asks server
        liftIO $ remote serv "ubigraph.new_edge" src dst
 
-setVertexAttribute :: ID -> String -> String -> Hubigraph Int
-setVertexAttribute id op val =
+removeEdge :: EdgeID -> Hubigraph Bool
+removeEdge eid =
     do serv <- asks server
-       liftIO $ remote serv "ubigraph.set_vertex_attribute" id op val
+       liftIO . toBool $ remote serv "ubigraph.remove_edge" eid
 
-vertexShape :: ID -> Shapes -> Hubigraph Int
-vertexShape node shape = setVertexAttribute node "shape" $ map toLower (show shape)
-
-vertexShapedetail :: ID -> Int -> Hubigraph Int
-vertexShapedetail node val = setVertexAttribute node "shapedetail" $ show val
-
-vertexColor :: ID -> String -> Hubigraph Int
-vertexColor node color = setVertexAttribute node "color" color
-
-vertexSize :: ID -> Float -> Hubigraph Int
-vertexSize node sz = setVertexAttribute node "size" $ show sz
-
-vertexLabel :: ID -> String -> Hubigraph Int
-vertexLabel node str = setVertexAttribute node "label" str
-
-vertexFontcolor :: ID -> String -> Hubigraph Int
-vertexFontcolor node color = setVertexAttribute node "fontcolor" color
-
-vertexFontfamily :: ID -> String -> Hubigraph Int
-vertexFontfamily node family = setVertexAttribute node "fontfamily" family
-
-vertexFontsize :: ID -> Int -> Hubigraph Int
-vertexFontsize node sz = setVertexAttribute node "fontsize" $ show sz
-
-vertexVisible :: ID -> Bool -> Hubigraph Int
-vertexVisible node bl = setVertexAttribute node "visible" $ map toLower (show bl)
-
-setEdgeAttribute :: ID -> String -> String -> Hubigraph Int
-setEdgeAttribute eid name val =
+-- #################
+setEdgeAttribute :: EdgeID -> EAttr -> Hubigraph Bool
+setEdgeAttribute eid ea =
     do serv <- asks server
-       liftIO $ remote serv "ubigraph.set_edge_attribute" eid name val
+       liftIO . toBool $ remote serv "ubigraph.set_edge_attribute"
 
-edgeArrow :: ID -> Bool -> Hubigraph Int
-edgeArrow eid bl = setEdgeAttribute eid "arrow" $ map toLower (show bl)
+changeVertexStyle :: VertexID -> StyleID -> Hubigraph Bool
+changeVertexStyle vid sid =
+    do serv <- asks server
+       liftIO . toBool $ remote serv "ubigraph.change_vertex_style" vid sid
 
-edgeArrowPosition :: ID -> Float -> Hubigraph Int
-edgeArrowPosition eid pos = setEdgeAttribute eid "arrow_position" $ show pos
+setVertexStyleAttribute :: StyleID -> VAttr -> Hubigraph Bool
+setVertexStyleAttribute sid va =
+    do serv <- asks server
+       liftIO . toBool $ remote serv "ubigraph.set_vertex_style_attribute" sid k v
+           where (k, v) = toPair va
 
-edgeArrowRadius :: ID -> Float -> Hubigraph Int
-edgeArrowRadius eid rad = setEdgeAttribute eid "arrow_radius" $ show rad
-
-edgeArrowLength :: ID -> Float -> Hubigraph Int
-edgeArrowLength eid len = setEdgeAttribute eid "arrow_length" $ show len
-
-edgeArrowReverse :: ID -> Bool -> Hubigraph Int
-edgeArrowReverse eid bl = setEdgeAttribute eid "arrow_reverse" $ map toLower (show bl)
-
-edgeColor :: ID -> String -> Hubigraph Int
-edgeColor eid color = setEdgeAttribute eid "color" color
-
-edgeLabel :: ID -> String -> Hubigraph Int
-edgeLabel eid str = setEdgeAttribute eid "label" str
-
-edgeFontcolor :: ID -> String -> Hubigraph Int
-edgeFontcolor eid color = setEdgeAttribute eid "fontcolor" color
-
-edgeFontfamily :: ID -> String -> Hubigraph Int
-edgeFontfamily eid family = setEdgeAttribute eid "fontfamily" family
-
-edgeFontsize :: ID -> Int -> Hubigraph Int
-edgeFontsize eid sz = setEdgeAttribute eid "fontsize" $ show sz
-
-edgeOriented :: ID -> Bool -> Hubigraph Int
-edgeOriented eid bl = setEdgeAttribute eid "oriented" $ map toLower (show bl)
-
-edgeSpline :: ID -> Bool -> Hubigraph Int
-edgeSpline eid bl = setEdgeAttribute eid "spline" $ map toLower (show bl)
-
-edgeShowstrain :: ID -> Bool -> Hubigraph Int
-edgeShowstrain eid bl = setEdgeAttribute eid "showstrain" $ map toLower (show bl)
-
-edgeStroke :: ID -> Stroke -> Hubigraph Int
-edgeStroke eid stroke = setEdgeAttribute eid "stroke" $ map toLower (show stroke)
-
-edgeStrength :: ID -> Float -> Hubigraph Int
-edgeStrength eid len = setEdgeAttribute eid "strength" $ show len
-
-edgeVisible :: ID -> Bool -> Hubigraph Int
-edgeVisible eid bl = setEdgeAttribute eid "visible" $ map toLower (show bl)
-
-edgeWidth :: ID -> Float -> Hubigraph Int
-edgeWidth eid width = setEdgeAttribute eid "width" $ show width
+newVertexStyle :: StyleID -> Hubigraph StyleID
+newVertexStyle sid =
+    do serv <- asks server
+       liftIO $ remote serv "ubigraph.new_vertex_style" sid
--- Hubigraph/Base.hs
+++ Hubigraph/Base.hs
@@ -1,18 +1,55 @@
 module Hubigraph.Base (
-   Hubigraph, Ubigraph(..), ID, Edge, Color, Shapes(..), Stroke(..),
+   Hubigraph, Ubigraph(..), VertexID, EdgeID, Edge, Color, Shape(..), Stroke(..),
+   Attr(..), VAttr(..), EAttr(..), StyleID,
   ) where
 
 import Control.Monad.Reader (ReaderT(..), runReaderT, asks, liftIO, lift)
-
+import Network.XmlRpc.Internals
 
 type Hubigraph = ReaderT Ubigraph IO
 data Ubigraph = Ubigraph { server :: String }
 
-type ID = Int
-type Edge = (Int, Int)
+type VertexID = Int
+type EdgeID   = Int
+type StyleID  = Int
+type Edge = (VertexID, VertexID)
+
 type Color = String
-data Shapes = Cone | Cube | Dodecahedron | Icosahedron
+
+data Shape = Cone | Cube | Dodecahedron | Icosahedron
             | Octahedron | Sphere | Tetrahedron
-            | Torus deriving (Show)
+            | Torus deriving Show
+
 data Stroke = Solid | Dashed | Dotted
-            | None deriving (Show)
+            | None deriving Show
+
+type VStyle = String
+type EStyle = String
+
+data VAttr = VColor Color
+           | VShape Shape
+           | VFontsize Int
+           | VVisible Bool
+
+data EAttr = EColor Color
+           | EShape Shape
+           | EStroke Stroke
+           | EFontsize Int
+           | EVisible Bool
+
+class Attr a where
+    toPair :: a -> (String, String)
+
+instance Attr VAttr where
+    toPair (VColor c)     = ("color",    c)
+    toPair (VShape Cone)  = ("shape",    "cone")
+    toPair (VShape Cube)  = ("shape",    "cube")
+    toPair (VShape _)     = ("shape",    "___")
+    toPair (VFontsize sz) = ("fontsize", show sz)
+
+instance Attr EAttr where
+    toPair (EColor c)     = ("color",    c)
+    toPair (EShape Cone)  = ("shape",    "cone")
+    toPair (EShape Cube)  = ("shape",    "cube")
+    toPair (EShape _)     = ("shape",    "___")
+    toPair (EFontsize sz) = ("fontsize", show sz)
