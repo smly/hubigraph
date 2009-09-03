4bf433569bfe38a4fd424495852a85ca6d7c8421
smly <smly@blei.(none)>
2009-09-03 20:33:10 +0900

    fix test

--- test/callback/callback_client.hs
+++ test/callback/callback_client.hs
@@ -3,4 +3,4 @@ r x = initHubigraph "http://localhost:20738/RPC2" >>= runHubigraph x
 main = do
   mapM_ (r . newVertexWithID) [1..3]
   mapM_ (r . (\n->setVAttr n (VCallback url))) [1..3]
-       where url = "http://localhost:19999/test_server.bin/vertex_callback"
+       where url = "http://localhost:19999/callback_serv.bin/vertex_callback"
